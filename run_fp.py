8281afae18135705f8bfd504e74ad9826a1a12e3
rishikiram <Rishtyang@gmail.com>
2024-11-15 14:23:26 -0700

    ready to run fingerprinting

--- run_fp.py
+++ run_fp.py
@@ -1,4 +1,5 @@
 from os import chdir
+import os
 import subprocess
 import argparse
 from parse_config import *
@@ -11,11 +12,13 @@ if __name__ == '__main__':
 	parser.add_argument('-c', '--config',
 		help='name of the global config file', default='config.json')
 	args = parser.parse_args()
+
 	config = parse_json(args.config)
 
 	# Get fingerprint parameter files
 	fp_params = []
 	param_dir = "../" + config["io"]["fp_param_dir"]
+	assert (os.path.isdir(param_dir)), "fp_param_dir in config.json is malformed"
 	fp_params = [param_dir + f for f in config["io"]["fp_params"]]
 
 	# Fingerprinting
@@ -34,6 +37,7 @@ if __name__ == '__main__':
 		"fp_param_dir": param_dir,
 		"fp_params": fp_params}
 	idx_config_fname = param_dir + "global_indices.json"
+	assert (os.path.isfile(idx_config_fname)), "no global_indices.json found in fp_param_dir"
 	process = subprocess.Popen((idxCommand % (idx_config_fname)),
 			stdout=subprocess.PIPE, shell=True)
 	output, error = process.communicate()
