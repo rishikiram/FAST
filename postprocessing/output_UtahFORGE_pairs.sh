8ecb864e8a2cb7d211b96dbe6b317b9cd6488aa0
rishikiram <Rishtyang@gmail.com>
2024-11-18 10:21:12 -0700

    typo

--- postprocessing/output_UtahFORGE_pairs.sh
+++ postprocessing/output_UtahFORGE_pairs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$ python parse_results.py -d ../data/waveformsFORK/fingerprints/ -p candidate_pairs_FORK_GHZ -i ../data/global_indices/FORK_GHZ_idx_mapping.txt
+python parse_results.py -d ../data/waveformsFORK/fingerprints/ -p candidate_pairs_FORK_GHZ -i ../data/global_indices/FORK_GHZ_idx_mapping.txt
