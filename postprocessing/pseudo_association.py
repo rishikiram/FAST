e7bb952b13e4d445a169843be569de29630c707d
rishikiram <Rishtyang@gmail.com>
2024-11-20 09:41:12 -0700

    silly

--- postprocessing/pseudo_association.py
+++ postprocessing/pseudo_association.py
@@ -55,8 +55,9 @@ class EventCloudExtractor:
             i = 0
             temp_time= time.time()
             while line_start - byte_pos < bytes_to_read:
-                if i % 10000000 == 0:
-                    print("%d in 10000000 passed in %d time" % (i,time.time()-temp_time))
+                i += 1
+                if i % 1000000 == 0:
+                    print("%d in 1000000 passed in %d time" % (i,time.time()-temp_time))
                     temp_time = time.time()
                 # no need to strip() since that's already done above
                 tmp = line.split()
